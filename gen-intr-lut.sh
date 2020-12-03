8920a39b9f39ef90afc118cf76911dda580f1b87
Jan Paw <jasiek.paw@gmail.com>
2020-12-03 20:11:08 +0100

    uppercase

--- gen-intr-lut.sh
+++ gen-intr-lut.sh
@@ -10,7 +10,7 @@ for intr_path in "$@"; do
     chip="$(basename "${intr_path%%.*}")"
     echo "        \"$chip\" => match intr {"
 
-    svd interrupts --no-gaps $intr_path | awk '{print "            \""substr($2, 1, length($2)-1)"\"" " => Some(" $1"),"}'
+    svd interrupts --no-gaps $intr_path | awk '{print "            \""toupper(substr($2, 1, length($2)-1))"\"" " => Some(" $1"),"}'
     echo "            _ => None,"
     echo "        },"
 done
