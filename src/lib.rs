066a428d76de155b5fe2bc3df25eac7d5a18f68d
Zachary Tomlinson <ztomlinson7@gmail.com>
2025-01-04 22:05:53 +0100

    Add ATmega16U2 and ATmega32U2

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@
 #![cfg_attr(feature = "atmega16", doc = "**atmega16**,")]
 #![cfg_attr(feature = "atmega164pa", doc = "**atmega164pa**,")]
 #![cfg_attr(feature = "atmega168", doc = "**atmega168**,")]
+#![cfg_attr(feature = "atmega16u2", doc = "**atmega16u2**,")]
 #![cfg_attr(feature = "atmega2560", doc = "**atmega2560**,")]
 #![cfg_attr(feature = "atmega8", doc = "**atmega8**,")]
 #![cfg_attr(feature = "atmega8u2", doc = "**atmega8u2**,")]
@@ -15,6 +16,7 @@
 #![cfg_attr(feature = "atmega328p", doc = "**atmega328p**,")]
 #![cfg_attr(feature = "atmega328pb", doc = "**atmega328pb**,")]
 #![cfg_attr(feature = "atmega32a", doc = "**atmega32a**,")]
+#![cfg_attr(feature = "atmega32u2", doc = "**atmega32u2**,")]
 #![cfg_attr(feature = "atmega32u4", doc = "**atmega32u4**,")]
 #![cfg_attr(feature = "atmega4808", doc = "**atmega4808**,")]
 #![cfg_attr(feature = "atmega4809", doc = "**atmega4809**,")]
@@ -63,6 +65,7 @@
 //! `atmega16`,
 //! `atmega164pa`,
 //! `atmega168`,
+//! `atmega16u2`,
 //! `atmega2560`,
 //! `atmega8`,
 //! `atmega8u2`,
@@ -70,6 +73,7 @@
 //! `atmega328p`,
 //! `atmega328pb`,
 //! `atmega32a`
+//! `atmega32u2`,
 //! `atmega32u4`,
 //! `atmega4808`,
 //! `atmega4809`,
@@ -232,11 +236,13 @@ compile_error!(
     * atmega16
     * atmega164pa
     * atmega168
+    * atmega16u2
     * atmega2560
     * atmega324pa
     * atmega328p
     * atmega328pb
     * atmega32a
+    * atmega32u2
     * atmega32u4
     * atmega4808
     * atmega4809
@@ -292,6 +298,8 @@ pub use crate::devices::atmega16;
 pub use crate::devices::atmega164pa;
 #[cfg(feature = "atmega168")]
 pub use crate::devices::atmega168;
+#[cfg(feature = "atmega16u2")]
+pub use crate::devices::atmega16u2;
 #[cfg(feature = "atmega2560")]
 pub use crate::devices::atmega2560;
 #[cfg(feature = "atmega324pa")]
@@ -302,6 +310,8 @@ pub use crate::devices::atmega328p;
 pub use crate::devices::atmega328pb;
 #[cfg(feature = "atmega32a")]
 pub use crate::devices::atmega32a;
+#[cfg(feature = "atmega32u2")]
+pub use crate::devices::atmega32u2;
 #[cfg(feature = "atmega32u4")]
 pub use crate::devices::atmega32u4;
 #[cfg(feature = "atmega4808")]
