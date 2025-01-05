26ad26bfb53efa50aa875a89844f9e3f3e0d3a2d
Paul Sbarra <Sbarra.Paul@gmail.com>
2025-01-05 11:27:40 -0600

    svdtools: install rust version
    This change removes a dependency on python/pip and is more consistent
    with the installation of the other supporting tools.

--- gen-intr-lut.sh
+++ gen-intr-lut.sh
@@ -10,7 +10,7 @@ for intr_path in "$@"; do
     chip="$(basename "${intr_path%.svd.patched}")"
     echo "        \"$chip\" => match intr {"
     # toupper() to be compliant with svd2rust interrupts name
-    svd interrupts --no-gaps $intr_path | awk '{print "            \""toupper(substr($2, 1, length($2)-1))"\"" " => Some(" $1"),"}'
+    svdtools interrupts --no-gaps $intr_path | awk '{print "            \""toupper(substr($2, 1, length($2)-1))"\"" " => Some(" $1"),"}'
     echo "            _ => None,"
     echo "        },"
 done
